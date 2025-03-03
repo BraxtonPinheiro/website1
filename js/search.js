15d2a5836ba3410584b957d1c6a692240e451ec7
BraxtonPinheiro <BraxtonPinheiro@gmail.com>
2025-03-03 06:15:49 +0000

    Sun, Mar 2, 2025, 10:15 PM -08:00

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
 btn = document.querySelector('.btn')
-search = document.querySelector('.search)
+search = document.querySelector('.search')
 
 
 btn.addEventListener('click', () =>{
-    search.classList.add('active')
+    search.classList.toggle('active')
 })
