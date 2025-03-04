65c11e8b35d3bed6da82c31f43972e3c23c75ac5
BraxtonPinheiro <BraxtonPinheiro@gmail.com>
2025-03-03 23:05:37 -0800

    Initial commit

--- js/loading.js
+++ js/loading.js
@@ -1,16 +1,19 @@
-const loadText = document.queryselection('.loading-text')
-const bg = document.queryselector('.bg')
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
 
 let load = 0
 
-let int = setInterval(bluring, 300)
+let int = setInterval(blurring, 30)
 
-function bluring() {
-    load++
+function blurring() {
+    load = load + 1
     if (load > 99){
         clearInterval(int)
     }
-    loadText.innerText = `$(load)%`
-    bg.style.filter = `blur($(50 - (load / 2))px)`
-    loadText.style.opacity = `$(1 - load/100)`
+    loadText.innerText = `${load}%`
+    bg.style.filter = `blur(${50 - load / 2}px)`
+    loadText.style.opacity = `${1 - load / 100}`
+
 }
+
+
