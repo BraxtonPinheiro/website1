7d1d4834254d4d76038295c6a5baee3545a472d8
BraxtonPinheiro <BraxtonPinheiro@gmail.com>
2025-03-05 23:57:47 +0000

    Wed, Mar 5, 2025, 3:57 PM -08:00

--- js/assignment1.js
+++ js/assignment1.js
@@ -1,7 +1,6 @@
 const button = document.querySelector('.btn')
 const box = document.querySelector('.box')
 const body = document.querySelector('body')
-button.textContent = "Change to Dark Mode";
 
 button.addEventListener('click', ()=> {
     button.classList.toggle('active')
@@ -9,7 +8,5 @@ button.addEventListener('click', ()=> {
     body.classList.toggle('active')
 })
 
-if (body.classList.contains('acitve', ()=> {
-    button.textContent = "Change to Light Mode";
-}))
+
 
