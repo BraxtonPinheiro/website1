a06509724b4591164e6085a9632e3f7352746fa7
BraxtonPinheiro <BraxtonPinheiro@gmail.com>
2025-03-06 00:07:21 +0000

    Wed, Mar 5, 2025, 4:07 PM -08:00

--- js/assignment1.js
+++ js/assignment1.js
@@ -1,11 +1,15 @@
 const button = document.querySelector('.btn')
 const box = document.querySelector('.box')
 const body = document.querySelector('body')
+const light = document.querySelector('.light')
+const dark = document.querySelector('.dark')
 
 button.addEventListener('click', ()=> {
     button.classList.toggle('active')
     box.classList.toggle('active')
     body.classList.toggle('active')
+    light.classList.toggle('active')
+    dark.classList.toggle('active')
 })
 
 
