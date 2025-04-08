b9a4169f045ab455eba92b1c2d4e243af5f258c5
BraxtonPinheiro <BraxtonPinheiro@gmail.com>
2025-04-08 17:04:53 +0000

    Tue, Apr 8, 2025, 10:04 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -2,6 +2,7 @@ const ball = document.createElement('div')
 document.body.appendChild(ball)
 const LPadel = document.createElement('div')
 document.body.appendChild(LPadel)
+
 let LPadelWidth = 20
 let LPadelHeight = 100
 let LPadelSpeed = 20
@@ -9,97 +10,90 @@ const ballRadius = 20
 const windowHeight = window.innerHeight
 let LPadelYPosition = windowHeight / 2 - LPadelHeight / 2
 const windowWidth = window.innerWidth
-let ballXPosition = windowWidth/2 - ballRadius
-let ballYPosition = windowHeight/2 - ballRadius
+let ballXPosition = windowWidth / 2 - ballRadius
+let ballYPosition = windowHeight / 2 - ballRadius
 let ballYDirection = 1
 let ballSpeed = 5
 let ballXDirection = 1
 
-
-
-function moveBall(){
-    ballXPosition = ballXPosition + ballSpeed * ballXDirection
-    ball.style.left = `${ballXPosition}px`
-    if (ballXPosition < 0 || ballXPosition > windowWidth - 2 * ballRadius){
-        ballXDirection = ballXDirection * -1
-    }
-    ballYPosition = ballYPosition + ballSpeed * ballYDirection
-    ball.style.top = `${ballYPosition}px`
-    if (ballYPosition < 0 || ballYPosition > windowHeight - 2 * ballRadius){
-        ballYDirection = ballYDirection * -1
-    }
-
-
-    let ballTop = ballYPosition
-    let ballBootom = ballYPosition + 2 * ballRadius
-    let ballLeft = ballXPosition
-    let LPadelTop = LPadelYPosition
-    let LpadelBottom = LPadelYPosition + LPadelHeight
-    let LPadelRight = LPadelXPosition + LPadelWidth
-
-
-    if(
-        (ballBottom >= LPadelTop) &&
-        (ballTop <= LPadelBottom) &&
-        (ballLeft <= LPadelRight) &&
-        (ballXDirection == -1)
-    )
-    {ballXDirection = ballXDirection * -1}
-
-
-}
-
-createBall()
-function createBall(){
+function createBall() {
     ball.style.height = `${2 * ballRadius}px`
     ball.style.width = `${2 * ballRadius}px`
     ball.style.borderRadius = '50%'
     ball.style.backgroundColor = 'blue'
     ball.style.position = 'absolute'
-    ball.style.top = `${windowHeight/2 - ballRadius}px`
-    ball.style.left = `${windowWidth/2 - ballRadius}px`
+    ball.style.top = `${windowHeight / 2 - ballRadius}px`
+    ball.style.left = `${windowWidth / 2 - ballRadius}px`
 }
 
-createLPadel()
-function createLPadel(){
+function createLPadel() {
     LPadel.style.height = `${LPadelHeight}px`
     LPadel.style.width = `${LPadelWidth}px`
     LPadel.style.backgroundColor = 'red'
     LPadel.style.position = 'absolute'
-    LPadel.style.left = '50px'
+    LPadel.style.left = '50px' // Fixed X position
     LPadel.style.top = `${LPadelYPosition}px`
 }
 
-
-wKey = false
-sKey = false
+let wKey = false
+let sKey = false
 
 document.addEventListener('keydown', (event) => {
-    if (event.key == 'w'){
+    if (event.key == 'w') {
         wKey = true
     }
-    if (event.key == 's'){
+    if (event.key == 's') {
         sKey = true
     }
 })
+
 document.addEventListener('keyup', (event) => {
-    if (event.key == 'w'){
+    if (event.key == 'w') {
         wKey = false
     }
-    if (event.key == 's'){
+    if (event.key == 's') {
         sKey = false
     }
 })
 
+function moveBall() {
+    ballXPosition = ballXPosition + ballSpeed * ballXDirection
+    ball.style.left = `${ballXPosition}px`
+    if (ballXPosition < 0 || ballXPosition > windowWidth - 2 * ballRadius) {
+        ballXDirection = ballXDirection * -1
+    }
+    ballYPosition = ballYPosition + ballSpeed * ballYDirection
+    ball.style.top = `${ballYPosition}px`
+    if (ballYPosition < 0 || ballYPosition > windowHeight - 2 * ballRadius) {
+        ballYDirection = ballYDirection * -1
+    }
+
+    // Ball-paddle collision detection
+    let ballTop = ballYPosition
+    let ballBottom = ballYPosition + 2 * ballRadius
+    let ballLeft = ballXPosition
+    let LPadelTop = LPadelYPosition
+    let LPadelBottom = LPadelYPosition + LPadelHeight
+    let LPadelRight = 50 + LPadelWidth // Fixed paddle's X position
+
+    if (
+        (ballBottom >= LPadelTop) &&
+        (ballTop <= LPadelBottom) &&
+        (ballLeft <= LPadelRight) &&
+        (ballXDirection == -1)
+    ) {
+        ballXDirection = ballXDirection * -1 // Ball hits the left paddle and bounces back
+    }
+}
 
 function moveLPadel() {
-    if(wKey == true && LPadelYPosition > 0) {
-        LPadelYPosition - LPadelYPosition - LPadelSpeed
+    if (wKey == true && LPadelYPosition > 0) {
+        LPadelYPosition -= LPadelSpeed // Corrected -= operator for up movement
     }
-    if(sKey ==true && LPadelYPosition < windowHeight - LPadelHeight) {
-        LPadelYPosition = LpadelYPosition + LPadelSpeed
+    if (sKey == true && LPadelYPosition < windowHeight - LPadelHeight) {
+        LPadelYPosition += LPadelSpeed
     }
-    LPaddel.style.top = `${LpadelYPosition}px`
+    LPadel.style.top = `${LPadelYPosition}px`
 }
 
 function animate() {
@@ -108,4 +102,6 @@ function animate() {
     requestAnimationFrame(animate)
 }
 
+createBall()
+createLPadel()
 animate()
