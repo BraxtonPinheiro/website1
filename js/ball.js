03d950b5aa3ba4f170a4d74da1e1efe1de146b99
BraxtonPinheiro <BraxtonPinheiro@gmail.com>
2025-04-08 17:03:52 +0000

    Tue, Apr 8, 2025, 10:03 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -15,15 +15,7 @@ let ballYDirection = 1
 let ballSpeed = 5
 let ballXDirection = 1
 
-let score = 0 // display the score, increase the score by 1
-// every time the ball hits the paddle
-let level = 1 //display the level, increase the level by 1
-//every time the score increases by 10
-//as the levels increase, increase ball speed
-//if the ball gets passed paddle, end the game.
-//make ball stop, or disapear and then let the user know
-//the game is over
-//optional: sound effects, background music
+
 
 function moveBall(){
     ballXPosition = ballXPosition + ballSpeed * ballXDirection
