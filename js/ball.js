df7cc316edebd89fe6fb93625ed1aa2905a09f99
BraxtonPinheiro <BraxtonPinheiro@gmail.com>
2025-04-11 16:26:03 +0000

    Fri, Apr 11, 2025, 9:26 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -39,7 +39,12 @@ function addScore() {
 
 displayScore()
 function displayScore() {
-    
+    scoreboard.innerText = `Score: ${score}`
+    scoreboard.style.fontSize = '2rem'
+    scoreboard.style.
+    scoreboard.style.position = 'absolute'
+    scoreboard.style.right = '0px'
+    scoreboard.style.lineHeight = '80px'
 }
 
 function createBall() {
