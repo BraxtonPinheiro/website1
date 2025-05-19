6b48efd1e0065d75098db9add7d753b1bb479bf5
BraxtonPinheiro <BraxtonPinheiro@gmail.com>
2025-05-19 15:55:36 +0000

    Mon, May 19, 2025, 8:55 AM -07:00

--- js/speech.js
+++ js/speech.js
@@ -1,7 +1,7 @@
 const main = document.querySelector('main')
 const voicesSelect = document.getElementById('voices')
 const textarea = document.getElementById('text')
-const readbtn = document.getElementById('read')
+const readBtn = document.getElementById('read')
 const toggleBtn = document.getElementById('toggle')
 const closeBtn = document.getElementById('close')
 
